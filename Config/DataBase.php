f3d61fefb68b65f491b57670e48284bf9af1f7d8
AmrIbrahem99 <amr2013hero@gmail.com>
2020-02-25 12:51:13 +0200

    intial commit

--- Config/DataBase.php
+++ Config/DataBase.php
@@ -1,5 +1,24 @@
 <?php
 
-class DataBase{
+class DataBase
+{
+    private $host = 'localhost';
+    private $db_name = 'bank el dawa2';
+    private $user_name = 'root';
+    private $password = '';
+    private $conn;
 
+    public function dbconnect()
+    {
+
+        $this->conn = null;
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->user_name, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $E) {
+            echo "connection error" . $E->getMessage();
+        }
+        return $this->conn;
+    }
 }
