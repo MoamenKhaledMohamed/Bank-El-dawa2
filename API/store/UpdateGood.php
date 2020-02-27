27b10bae56b33a8554eb93c04179e2f23a16b84e
MoamenKhaledMohamed <mohmanhessien2.2@gmail.com>
2020-02-27 11:17:17 +0200

    Add Update API

--- API/store/UpdateGood.php
+++ API/store/UpdateGood.php
@@ -1,1 +1,33 @@
 <?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../Config/Database.php';
+    include_once '../../Models/Store.php';
+
+    // connect to data base
+    $Conn = new DataBase();
+    $db = $Conn->dbconnect();
+
+    // create Store
+    $store = new Store($db);
+
+    //Get Data From Body OF Header
+    $data =  json_decode(file_get_contents("php://input"));
+
+    // Put Data To Attributes
+    $store->NameTool = $data->NameTool;
+    $store->TypeTool = $data->TypeTool;
+    $store->Quantity = $data->Quantity;
+    $store->IdTool = $data->IdTool;
+    $store->DateIn = $data->DateIn;
+    $store->NameOfDonner = $data->NameOfDonner;
+    $store->IdDonner = $data->IdDonner;
+
+    //Update
+    $store->update();
+
+
--- Models/Store.php
+++ Models/Store.php
@@ -7,6 +7,8 @@ class Store extends Model {
     public $TypeTool;
     public $Quantity;
     public $DateIn;
+    public $NameOfDonner;
+    public $IdDonner;
 
     public function __construct ($db){
         parent::__construct($db);
@@ -23,16 +25,46 @@ class Store extends Model {
 
          return $stmt;
 
-
-
-
     }
 
     public function update()
     {
-        // TODO: Implement update() method.
-    }
+        try {
+            // Start Transaction
+            $this->Conn->beginTransaction();
 
+            //Update Tools Table
+            $stmt = $this->Conn->prepare("UPDATE tools SET Name_Tool = ?,TypeTool = ?,Quantatiy = ? WHERE ID_Tool = ?");
+            $stmt->execute([
+                $this->NameTool,
+                $this->TypeTool,
+                $this->Quantity,
+                $this->IdTool
+            ]);
+
+            // update DataIn in Tools_Given Table
+            $stmt = $this->Conn->prepare("UPDATE  tools_given SET DateIN = ? WHERE ID_Tool_Given = ?");
+            $stmt->execute([$this->DateIn, $this->IdTool]);
+
+            //UPDATE Name Of Donner
+            $stmt = $this->Conn->prepare("UPDATE persons SET Name = ? WHERE ID = ?");
+            $stmt->execute([$this->NameOfDonner, $this->IdDonner]);
+
+            // Commit
+            $this->Conn->commit();
+
+            // print updated
+            echo json_encode(["Message" => "UPDATED"]);
+        }
+        catch (PDOException $e) {
+
+            // check if one of Transaction Not Exec Will Rollback
+            $this->Conn->rollback();
+            echo $e->getMessage();
+
+        }
+
+    }
     public function delete()
     {
         // TODO: Implement delete() method.
